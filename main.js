7adb671641a8410f201a87605a541de49c80bb35
chris0810 <you@example.com>
2023-04-27 17:27:00 +0200

    icons addet to sights

--- main.js
+++ main.js
@@ -119,6 +119,17 @@ async function showSights(url){
     L.geoJSON(jsondata).addTo(themaLayer.sights)
     //console.log(response, jsondata)
     L.geoJSON(jsondata, {
+        pointToLayer: function(feature, latlng) {
+            L.marker(latlng).addTo(map)
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: 'icons/photo.png',
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37],
+
+                })
+            });
+        },
         onEachFeature: function(feature, layer){
             let prop = feature.properties;
             layer.bindPopup(`
