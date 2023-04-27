6ea172dc695b6e8a6bb75b53874bf2d8a558b600
chris0810 <you@example.com>
2023-04-27 13:31:51 +0200

    new

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let stephansdom = {
 let map = L.map("map").setView([
     stephansdom.lat, stephansdom.lng
 ], 12);
-map.addControl(new L.Control.Fullscreen());
+    map.addControl(new L.Control.Fullscreen());
 //thematische Layer
 let themaLayer = {
     stops: L.featureGroup().addTo(map),
